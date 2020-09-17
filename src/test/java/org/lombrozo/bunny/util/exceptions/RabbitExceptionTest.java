5b9a5302ba02b4eb59db4e6f4526ad272e44780f
Lombrozo <you@example.com>
2020-09-17 15:09:50 +0300

    Rabbit Exception testing

---
+++ src/test/java/org/lombrozo/bunny/util/exceptions/RabbitExceptionTest.java
@@ -0,0 +1,20 @@
+package org.lombrozo.bunny.util.exceptions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RabbitExceptionTest {
+
+    @Test
+    public void exceptionInfoTest() {
+        String expectedMessage = "Message";
+        try {
+            throw new RabbitException(new Exception(expectedMessage));
+        } catch (RabbitException e) {
+            assertNotNull(e);
+            assertEquals(expectedMessage, e.getCause().getMessage());
+        }
+    }
+
+}
