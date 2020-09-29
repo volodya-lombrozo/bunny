2ee5f9569a36a84335b7a3f9a1b5d514c61b41cf
Lombrozo <you@example.com>
2020-09-29 13:11:14 +0300

    Try to commit failed test

--- src/test/java/org/lombrozo/bunny/client/RabbitClientTest.java
+++ src/test/java/org/lombrozo/bunny/client/RabbitClientTest.java
@@ -31,7 +31,7 @@ public class RabbitClientTest {
     @Test(timeout = 100)
     public void send_queueNamesOnly_successfulSendAndReceive() throws RabbitException {
         RabbitClient client = new RabbitClient(connection, replyQueueName);
-        Message message = new RPCMessage("hello", new ReplyTo("." + replyQueueName));
+        Message message = new RPCMessage("hello", new ReplyTo("1." + replyQueueName));
         QueueDestination destination = new QueueDestination(new NamedQueue(sendQueueName, connection));
 
         new ResponsibleQueueConsumer(sendQueueName, connection)
